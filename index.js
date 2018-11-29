64f69a5fe853254bf444043b39e72ba83249d462
Dipsushi <32496505+Dipsushi@users.noreply.github.com>
2018-11-29 16:21:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,9 +26,24 @@ bot.on("message", async message => {
  if(cmd === `${prefix}register`) { 
    players[a] = message.member;
    bank[b] = Math.round(200);
-   return message.channel.send(players[a] + " have successfully registered! You have $" + bank[b] + "in your bank account!"); 
+   return message.channel.send(players[a] + " have successfully registered! You have $" + bank[b] + " in your bank account!"); 
    a++;
    b++;
+ }
+ function getRandomInt(min, max) {
+ min = Math.ceil(min);
+ max = Math.floor(max);
+ return Math.floor(Math.random() * (max - min)) + min;
+ }
+ if(message.content === `${prefix}random`) {
+   var x = getRandomInt(0 , 10);
+   if(x =< 5) {
+     var A = bank[players.indexOf(message.member)] - 50;
+     bank[players.indexOf(message.member)] = A; 
+     return message.channel.send("You lost $50. Your new balance is now $ " + A + ".");
+   } else if(x > 5 && x =< 10){
+     return message.channel.send("You are trash");
+   } 
  }
 
 });   
