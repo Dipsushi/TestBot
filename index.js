3d9a1a3f59bd80a639940781107e4aaf229ce94e
Dipsushi <32496505+Dipsushi@users.noreply.github.com>
2018-11-30 00:29:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,6 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-
- var players = [];
- var bank = [];
- var a = 0;
- var b = 0;
- var value = 0;
- var loss = 50;
   
  if(cmd === `${prefix}register`) {
    if(players.includes(message.member) == -1) {
