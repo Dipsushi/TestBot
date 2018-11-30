af58fb6c593d3c729dcf9f52bca62fd873ff4fea
Dipsushi <32496505+Dipsushi@users.noreply.github.com>
2018-11-30 00:26:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,12 +17,6 @@ bot.on("message", async message => {
   let messageArray = message.content.split(" ");
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
-  
-  function getRandomInt(min, max) {
- min = Math.ceil(min);
- max = Math.floor(max);
- return Math.floor(Math.random() * (max - min)) + min;
- } 
 
  var players = [];
  var bank = [];
@@ -41,7 +35,19 @@ bot.on("message", async message => {
  } else {
    return message.channel.send("You are already registered!");
  }
-   
+   function getRandomInt(min, max) { 
+     
+ var players = [];
+ var bank = [];
+ var a = 0;
+ var b = 0;
+ var value = 0;
+ var loss = 50;
+     
+ min = Math.ceil(min);
+ max = Math.floor(max);
+ return Math.floor(Math.random() * (max - min)) + min;
+ } 
  if(message.content === `${prefix}money`) {
    var x = getRandomInt(0 , 10);
    if(x <= 5) {
