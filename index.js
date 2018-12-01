17ce584e3b0b707b96ba38aa13a94f624a39a16f
Dipsushi <32496505+Dipsushi@users.noreply.github.com>
2018-11-30 23:28:04 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const {Client, RichEmbed} = require(`discord.js`);
 
+const fs = require("fs");
+bot.data = require("./data.json");
+
 const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("ready", async () => {
@@ -18,29 +21,17 @@ bot.on("message", async message => {
   let cmd = messageArray[0];
   let args = messageArray.slice(1);
    
-  var players = new Array(5);
-  var bank = new Array(5); 
-  var a = 0;
-  var index = 0;
-  var loss = 50;
-  var value = 0;
-  var newvalue = 0; 
-  
-  if(cmd === `${prefix}start`) { 
-      if(players.includes(message.member) = -1) {
-        players.splice(a, 0, message.member); 
-        bank.splice(a, 0, 200); 
-        
-        return message.channel.send(players[a-1] + " " + bank[a-1]); 
-      } else {
-        index = players.indexOf(message.member); 
-        value = bank[index]; 
-        newvalue = value + loss; 
-        bank.splice(index, 1, newvalue); 
-        
-        return message.channel.send(bank[index]); 
+  if(message.content.startsWith("write")) {
+    editmessage = messag.content.slice(6); 
+    
+    bot.data [message.author.username] = {
+      message: editmessage
   }
- }
-       
+  fs.writeFile("./data.json", JSON.stringify (bot.data, null, 4), err => {
+    if(err) throw err;
+    message.channelsend("message written");
+      }
+     }
+ 
 });   
 bot.login(botconfig.token);
